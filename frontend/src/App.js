3e2c741d823ad497b8bae26622d92349310979ca
B Karthik <bangikarthik7@gmail.com>
2025-01-16 20:06:36 +0530

    title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,7 +39,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 py-4">
         <div className="flex justify-between items-center">
           <h1 className="text-3xl font-extrabold text-white">
-            <Link to="/submissions">Glimpse</Link>
+            <Link to="/submissions">Interview Experience Platform( Glimpse )</Link>
           </h1>
           <div className="flex items-center space-x-4">
             {token ? (
